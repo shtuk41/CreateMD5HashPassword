2c16c3c2f8fc131dfc9418c2594f6353355837b4
Aleksander <shtuk41@hotmail.com>
2019-08-03 05:53:41 -0500

    initial commit

---
+++ CreateMD5HashPassword/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CreateMD5HashPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generate = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMD5Hash = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(404, 56);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(129, 34);
+            this.Generate.TabIndex = 0;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(21, 62);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(322, 22);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MD5 Hash";
+            // 
+            // textBoxMD5Hash
+            // 
+            this.textBoxMD5Hash.Location = new System.Drawing.Point(27, 187);
+            this.textBoxMD5Hash.Name = "textBoxMD5Hash";
+            this.textBoxMD5Hash.Size = new System.Drawing.Size(506, 22);
+            this.textBoxMD5Hash.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 340);
+            this.Controls.Add(this.textBoxMD5Hash);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.Generate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMD5Hash;
+    }
+}
+
---
+++ CreateMD5HashPassword/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Globalization;
+
+namespace CreateMD5HashPassword
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _Salt = "{0C0F1151-CF4D-4b1a-94E3-306AA197AE22}";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+
+            byte[] data;
+            using (MD5 hasher = MD5.Create())
+            {
+                data = hasher.ComputeHash(Encoding.Default.GetBytes(_Salt + textBoxPassword.Text));
+            }
+
+            // reformat data, we want to return a hexadecimal string
+            var builder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2", CultureInfo.InvariantCulture));
+            }
+
+           textBoxMD5Hash.Text = builder.ToString();
+        }
+    }
+}
